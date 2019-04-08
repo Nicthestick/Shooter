5e8d2949bf7e049bb752e51ce87572a56ab9190f
Nicolas <nicolaspaulmusic@gmail.com>
2019-04-08 23:30:22 +0200

    added photon view and photon transform view to the project. This version is the first to implement smooth motion over the network. Cubes when the player enters the game. no player controller yet

---
+++ Shooter2.0/Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,72 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+
+    public static PhotonLobby lobby;
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        lobby = this; //creates singleton
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //connects to photon server
+    }
+
+
+    public void OnCancelButtonClicked()
+    {
+        battleButton.SetActive(true);
+        cancelButton.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleClicked()
+    {
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log("Failed to join random room. No rooms available. Creating Room.");
+        CreateRoom();
+       
+    }
+
+    void CreateRoom()
+    {
+        
+        int randRoom = Random.Range(0, 10000);
+        RoomOptions roomops = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room", roomops);
+        Debug.Log("Room " + randRoom + " successfully created");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room Create Failed. Trying again.");
+        CreateRoom();
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+}
---
+++ Shooter2.0/Assets/Scripts/Photon/PhotonRoom.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+
+    public static PhotonRoom room;
+    private PhotonView PV;
+
+    public int multiplayerScene;
+    public int currentScene;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (PhotonRoom.room == null)
+        {
+            PhotonRoom.room = this;
+        }
+        else
+        {
+            if(PhotonRoom.room != this)
+            {
+                Destroy(PhotonRoom.room.gameObject);
+                PhotonRoom.room = this;
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+        PV = GetComponent<PhotonView>();
+    }
+
+
+    public void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.buildIndex;
+        if(currentScene == multiplayerScene)
+        {
+            CreatePlayer();
+        }
+    }
+
+    private void CreatePlayer()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonNetworkPlayer"), transform.position, Quaternion.identity);
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+        SceneManager.sceneLoaded -= OnSceneFinishedLoading;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Successfully joined room");
+
+        StartGame(); 
+    }
+
+    void StartGame()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+
+        PhotonNetwork.LoadLevel(multiplayerScene); 
+    }
+}
